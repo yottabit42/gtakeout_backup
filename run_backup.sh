43ebbd376a2ed0c1226bb24c533e94f1c1857422
Jacob McDonald <jacob@mcawesome.org>
2019-10-08 21:08:16 -0500

    Cleanup commenting.

--- run_backup.sh
+++ run_backup.sh
@@ -12,7 +12,7 @@ host_id="root@172.16.42.25"
 unix_seconds=$(date +%s)
 ram_buffer="4G"
 
-#
+###
 # This script performs the following actions:
 #
 # 1. Extract the tgz archive(s) defined as ${archive}, over top of the
@@ -34,19 +34,19 @@ ram_buffer="4G"
 #
 #  1. bash: preferred shell compatible with this script.
 #  2. mbuffer: optimize the speed of extract, especially on systems with a lot
-#    of RAM and slow disks.
+#     of RAM and slow disks.
 #  3. pigz: optimize the speed of extract, especially on systems with a lot of
-#    CPU threads.
+#     CPU threads.
 #  4. find: enumerate the archives for the extract loop.
 #  5. tar: required for extraction of the data.
 #  6. gsutil: required to push new and changed data to GCS.
 #  7. Persistent authentication key for GCS.
 #  8. Passwordless SSH key to operate remote ZFS commands as root, if you want
-#    to maximize automation.
+#     to maximize automation.
 #  9. Google Takeout archive(s) must be in tgz (tar) format.
 # 10. All tgz archives in the path will be used, so you probably want to place
-#    them in a unique subdir.
-#
+#     them in a unique subdir.
+###
 
 echo "Started at $(date)."
 
