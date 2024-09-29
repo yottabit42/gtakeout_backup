e6102458a7c0af4757606f06f5a7f6f32b331ddc
Jacob McDonald <jacob@mcawesome.org>
2024-09-29 18:27:06 -0500

    Update get_takeout.sh

--- get_takeout.sh
+++ get_takeout.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
-#
+# get_takeout.sh by @yottabit42 (Jacob McDonald), rev. 2024-09-29 #1.
+
+echo "Usage:"
+echo
+echo "${0}"
+echo
+echo "Prompts for cURL(s) to download in parallel, using server-supplied"
+echo "filenames, modifying if necessary to prevent overwriting existing"
+echo "files with the same name. Up to 50 cURL entries are allowed. An empty"
+echo "entry ends the series."
+echo
+echo "Note: you probably should not use 50 cURLs without really knowing what"
+echo "      you are doing. Typical home and business networks and machines can"
+echo "      handle between 3 and 10 simultaneous downloads in parallel."
+echo
 
 if [[ ! $(which curl) ]]; then
   echo "Please install 'curl' first, e.g., 'sudo apt install curl'. Exiting."
@@ -9,49 +23,30 @@ if [[ ! $(which cut) ]]; then
   echo "Please install 'cut' first, e.g., 'sudo apt install cut'. Exiting."
   exit
 fi
-if [[ "${1}" ]]; then
-  if [[ ! "${1}" =~ ^[0-9]+$ ]]; then
-    echo "<NUM_DL> must be a whole number. Exiting."
-    exit
-  elif (( "${1}" > 50 )) || (( "${1}" < 1 )); then
-    echo "<NUM_DL> must be between 1 and 50. Exiting."
-    exit
-  fi
-  for (( i=0; i<=$(("${1}"-1)); i++ )); do
-    echo -n "Paste cURL, then press Enter twice: "
-    while IFS='' read -r; do
-      if [[ -z "${REPLY}" ]]; then
-        break
-      fi
-      cURL[i]+="${REPLY}"
-    done
-    cURL[i]="${cURL[i]//curl /}"
-    cURL[i]="${cURL[i]//[$'\t\r\n\\']}"
-    if [[ -z "${cURL[i]}" ]]; then
+for (( i=0; i<=49; i++ )); do
+  echo -n "Paste cURL, then press Enter, or Enter alone to end: "
+  while IFS='' read -r; do
+    if [[ "${REPLY: -1}" != '\' || -z "${REPLY}" ]]; then
       break
     fi
+    cURL[i]+="${REPLY}"
   done
-  if [[ -z "${cURL[0]}" ]]; then
-    exit
+  cURL[i]="${cURL[i]//curl /}"
+  cURL[i]="${cURL[i]//[$'\t\r\n\\']}"
+  if [[ -z "${cURL[i]}" ]]; then
+    break
   fi
-  for (( i=1; i<=$(("${1}"-1)); i++ )); do
-    if [[ ! -z "${cURL[i]}" ]]; then
-      cURL[i]+=" "
-      cURL[i]+=$(echo "${cURL[i]}" | cut -d ' ' -f 1)
-    fi
-  done
-  trapCmd="echo -e '\nAn error occurred during download. Check your files:'; "
-  trapCmd+="ls -lh; exit 1"
-  trap "${trapCmd}" ERR
-  eval curl --remote-name-all --parallel-immediate -JLOZf ${cURL[@]}
-else
-  echo "Usage:"
-  echo
-  echo "${0} <NUM_DL>"
-  echo
-  echo "Prompts for cURL(s) to download in parallel, using server-supplied"
-  echo "filenames, overwriting if necessary. An empty cURL entry ends the"
-  echo "series early (safely)."
-  echo
-  echo "<NUM_DL>  Number of parallel downloads, from 1 to 50"
+done
+if [[ -z "${cURL[0]}" ]]; then
+  exit
 fi
+for (( i=1; i<=$(("${1}"-1)); i++ )); do
+  if [[ ! -z "${cURL[i]}" ]]; then
+    cURL[i]+=" "
+    cURL[i]+=$(echo "${cURL[i]}" | cut -d ' ' -f 1)
+  fi
+done
+trapCmd="echo -e '\nAn error occurred during download. Check your files:'; "
+trapCmd+="ls -lh; exit 1"
+trap "${trapCmd}" ERR
+eval curl --remote-name-all --parallel-immediate -JLOZf ${cURL[@]}
