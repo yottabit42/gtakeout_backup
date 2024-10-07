130d6738e14736a38128931a7e402c4ed9365e7b
Jacob McDonald <jacob@mcawesome.org>
2024-10-06 19:21:31 -0500

    Update get_takeout.sh

--- get_takeout.sh
+++ get_takeout.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-# get_takeout.sh by @yottabit42 (Jacob McDonald), rev. 2024-09-29 #1.
+# get_takeout.sh by @yottabit42 (Jacob McDonald), rev. 2024-10-06 #1.
 
 echo "Usage:"
 echo
 echo "${0}"
 echo
 echo "Prompts for cURL(s) to download in parallel, using server-supplied"
-echo "filenames, overwriting existing files with the same name. Up to 50 cURL"
-echo "entries are allowed. An empty entry ends the series."
+echo "filenames, modifying if necessary to prevent overwriting existing"
+echo "files with the same name. Up to 50 cURL entries are allowed. An empty"
+echo "entry ends the series."
 echo
 echo "Note: you probably should not use 50 cURLs without really knowing what"
 echo "      you are doing. Typical home and business networks and machines can"
@@ -26,12 +27,13 @@ for (( i=0; i<=49; i++ )); do
   echo -n "Paste cURL, then press Enter, or Enter alone to end: "
   while IFS='' read -r; do
     if [[ "${REPLY: -1}" != '\' || -z "${REPLY}" ]]; then
+      cURL[i]+="${REPLY}"
       break
     fi
     cURL[i]+="${REPLY}"
   done
   cURL[i]="${cURL[i]//curl /}"
-  cURL[i]="${cURL[i]//[$'\t\r\n\\']}"
+  cURL[i]="${cURL[i]//[$'\t\r\n\\']/}"
   if [[ -z "${cURL[i]}" ]]; then
     break
   fi
@@ -39,13 +41,13 @@ done
 if [[ -z "${cURL[0]}" ]]; then
   exit
 fi
-for (( i=1; i<=$(("${1}"-1)); i++ )); do
+for (( i=1; i<=$(("${#cURL[@]}"-1)); i++ )); do
   if [[ ! -z "${cURL[i]}" ]]; then
-    cURL[i]+=" "
-    cURL[i]+=$(echo "${cURL[i]}" | cut -d ' ' -f 1)
+    cURL[0]+=" "
+    cURL[0]+=$(echo "${cURL[i]}" | cut -d ' ' -f 1)
   fi
 done
 trapCmd="echo -e '\nAn error occurred during download. Check your files:'; "
 trapCmd+="ls -lh; exit 1"
 trap "${trapCmd}" ERR
-eval curl --remote-name-all --parallel-immediate -CJLOZf ${cURL[@]}
+eval curl --remote-name-all --parallel-immediate -JLOZ ${cURL[0]}
